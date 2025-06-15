c4c69baaede28b8ed9f0db82539b75067b809030
Anatolii Kostanda <kosttolis@gmail.com>
2025-06-15 14:32:09 +0300

    added list, update and delete views

--- tasks/models.py
+++ tasks/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Task(models.Model):
     POSSIBLE_STATUSES = (
         ('pending', 'Pending'),
--- tasks/urls.py
+++ tasks/urls.py
@@ -2,16 +2,16 @@ from django.urls import path
 
 from .views import (
     TaskCreateView,
+    TaskListView,
+    TaskUpdateView,
+    TaskDeleteView,
 )
 
 urlpatterns = [
     path('create', TaskCreateView.as_view(), name='task-create'),
-    # path('create/', EventCreateAPIView.as_view(), name='event-creat'),
-    # path('', EventListAPIView.as_view(), name='event-list'),
-    # path('<int:pk>/', EventDetailAPIView.as_view(), name='event-detail'),
-    # path('<int:pk>/update/', EventUpdateAPIView.as_view(), name='event-update'),
-    # path('<int:pk>/delete/', EventDeleteAPIView.as_view(), name='event-delete'),
-    # path('<int:pk>/add_participant/', EventAddParticipantAPIView.as_view(), name='event-add-participant'),
+    path('list/', TaskListView.as_view(), name='task-list'),
+    path('update/<int:pk>', TaskUpdateView.as_view(), name='task-update'),
+    path('delete/<int:pk>', TaskDeleteView.as_view(), name='task-delete'),
 
 ]
 
